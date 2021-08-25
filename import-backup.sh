414b0fe1cf4b2d397180cc0b66b7c7ad544eb27e
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-08-25 00:23:37 -0400

    Create import-backup.sh

---
+++ import-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Gr33nDrag0n 2021-08-02
+
+if [ $( ps -ef | grep "lisk-core start" | grep -v grep ) -ge 1 ]
+then
+    echo "The lisk-core process is running. Stop it and retry."
+    exit 1
+fi
+
+echo "Sync. Git Repository."
+cd "$HOME/forger-sync/"
+git pull --rebase --quiet
+
+echo "Select Backup to Import."
+cd "$HOME/forger-sync/backup/"
+select Directory in *
+do
+    test -n "$Directory" && break
+    echo ">>> Invalid Selection"
+done
+
+BackupFilePath="$HOME/forger-sync/backup/$Directory/forger.db.tar.gz"
+
+echo "Importing: $BackupFilePath"
+"$HOME/lisk-core/bin/lisk-core" forger-info:import "$BackupFilePath"
