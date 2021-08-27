fb915d64bcf3c9fd02d829a47ec28f1a4c48b431
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-08-26 23:27:05 -0400

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Gr33nDrag0n 2021-08-02
+# Gr33nDrag0n 2021-08-26
 
-if [ $( ps -ef | grep 'forger-sync/logmonitor.sh' | grep -v grep | wc -l ) -lt 1 ]
+ProcessCount=$( ps -ef | grep 'forger-sync/logmonitor.sh' | grep -v grep | wc -l )
+if [ "$ProcessCount" -lt 1 ]
 then
     "$HOME/forger-sync/start-logmonitor.sh"
 fi
